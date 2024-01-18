9514f0b81358a28c4d5161a079cf547a24f2fbb5
Wes Benvindo <wal2.designer@gmail.com>
2024-01-18 19:13:41 +0000

    adding new solution

---
+++ 01_exercises/ListsExercises/ListsExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsExercises
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ask to user enter a name until the neme is ""
+            var friends = new List<string>();
+
+
+            while (true)
+            {
+                Console.WriteLine("Entre com o nome de um amigo:");
+                var name = Console.ReadLine();
+
+                if (name != "")
+                {
+                    friends.Add(name);
+                    continue;
+                }
+
+                break;
+            }
+
+            var amountOfFriends = friends.Count;
+
+            if (friends.Count > 0)
+            {
+                for (var i = 0; i < friends.Count; i++)
+                {
+                    if (friends.IndexOf(friends[i]) < 2)
+                    {
+                        Console.WriteLine(friends[i]);
+                    }
+                }
+
+                if (amountOfFriends > 2)
+                {
+                    Console.WriteLine(String.Format("+ {0}", amountOfFriends - 2));
+                }
+            }
+        }
+    }
+}
